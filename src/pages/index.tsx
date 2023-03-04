09f0e53615cc211e4b33dd5ae5fbd3076525905e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-03-04 23:28:02 +0100

    remove hello

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,6 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -18,12 +16,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-3xl text-white">Auth demo</h1>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
-            <AuthShowcase />
-          </div>
+          <AuthShowcase />
           <ul className="text-white">
             <li className="flex flex-col">
               <Link href="/00-context">- Context</Link>
