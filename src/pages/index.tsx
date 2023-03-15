70673d917f219733f1415387403f4825b5cf75e4
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-03-15 20:59:40 +0100

    naming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const { data: sessionData } = useSession();
+  const { data: session } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+  const secretMessage = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined }
+    { enabled: session?.user !== undefined }
   );
 
   return (
@@ -25,16 +25,14 @@ const Home: NextPage = () => {
           <h1 className="text-3xl text-white">Auth demo</h1>
           <div className="flex flex-col items-center justify-center gap-4">
             <p className="text-center text-2xl text-white">
-              {sessionData && (
-                <span>Logged in as {sessionData.user?.name}</span>
-              )}
-              {secretMessage && <span> - {secretMessage}</span>}
+              {session && <span>Logged in as {session.user?.name}</span>}
+              {secretMessage.data && <span> - {secretMessage.data}</span>}
             </p>
             <button
               className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-              onClick={sessionData ? () => void signOut() : () => void signIn()}
+              onClick={session ? () => void signOut() : () => void signIn()}
             >
-              {sessionData ? "Sign out" : "Sign in"}
+              {session ? "Sign out" : "Sign in"}
             </button>
           </div>
           <ul className="text-white">
